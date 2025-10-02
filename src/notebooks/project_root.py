8bac7a45b9b2d1bc5eb6bd22195c081faead03c0
henryzhou7 <zhouyh.7@gmail.com>
2025-10-01 20:20:03 -0700

    pydotenv

--- src/notebooks/project_root.py
+++ src/notebooks/project_root.py
@@ -1,6 +1,18 @@
+"""
+This module is used to set the project root and load the .env file.
+"""
+
 import os
+from dotenv import load_dotenv, find_dotenv
+
 
 while os.getcwd().endswith("notebooks"):
     os.chdir("..")
 
 print(os.getcwd())
+
+# Look for the nearest .env starting from the CWD
+env_path = find_dotenv(usecwd=True)
+if env_path:
+    print("found .env file")
+    load_dotenv(env_path, override=False)  #
