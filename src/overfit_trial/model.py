6b701bd3400af8881b4722981f7f4718d84ee490
henryzhou7 <zhouyh.7@gmail.com>
2025-10-04 09:52:42 -0700

    backbone forward pass

--- src/overfit_trial/model.py
+++ src/overfit_trial/model.py
@@ -1,16 +1,26 @@
 """
 Implementation of the model.
 """
+# ruff: noqa: F722
+
+from __future__ import annotations
 
 import torch
 import torch.nn as nn
 from transformers import MimiModel
+from jaxtyping import Float
 
-from .model_utils import (
+from overfit_trial.model_utils import (
     prepare_transformer,
     FLAVORS,
     load_llama_weights,
 )
+
+MODEL_ID = "llama-1B"
+
+
+def _create_causal_mask(seq_len: int, device: torch.device = torch.device("cpu")):
+    return torch.tril(torch.ones(seq_len, seq_len, dtype=torch.bool, device=device))
 
 
 class MachOverfitModel(nn.Module):
@@ -19,9 +29,13 @@ class MachOverfitModel(nn.Module):
 
         self.input_dim = input_dim
         self.num_quantizers = num_quantizers
+
         self._init_quantizer(num_quantizers)
-        self._init_backbone()
+        self._init_backbone(MODEL_ID)
         self._init_decoder()
+
+        #
+        self.causal_mask = _create_causal_mask(self.backbone.max_seq_len)
 
     def _init_quantizer(self, num_quantizers: int):
         """
@@ -38,12 +52,13 @@ class MachOverfitModel(nn.Module):
         The backbone LLM of the model.
         """
         if model_id.startswith("llama"):
-            self.backbone, embed_dim = prepare_transformer(FLAVORS["llama-1B"]())
-            self.backbone = load_llama_weights(self.backbone, model_id)
+            backbone = load_llama_weights(model_id)
+            self.backbone, embed_dim = prepare_transformer(backbone)
         else:
             raise ValueError(f"Model ID {model_id} not supported")
 
-        self.input_proj = nn.Linear(self.input_dim, embed_dim)
+        # Need to convert the two channel inputs into the input to the transformer.
+        self.input_proj = nn.Linear(self.input_dim * 2, embed_dim)
 
     def _init_decoder(self):
         """
@@ -54,8 +69,44 @@ class MachOverfitModel(nn.Module):
     def forward(self, codes_c1: torch.Tensor, codes_c2: torch.Tensor):
         """
         Forward pass of the model.
+            codes_c1 / codes_c2: (1, num_quantizers, num_frames)
+                num_frames is sampled at 12.5Hz.
 
         The forward pass consists of two steps
             1. takes in the two channels of RVQ codes and outputs the latent features
             2. the latent features are passed through the decoder to predict the RVQ codes
         """
+        bs = codes_c1.shape[0]
+        T = codes_c1.shape[2]
+        latent_features: Float[torch.Tensor, "bs T embed_dim"] = (
+            self._embed_audio_codes(codes_c1, codes_c2)
+        )
+
+        mask = self.causal_mask.unsqueeze(0).expand(bs, -1, -1)[:, :T, :T]
+
+        # transformer forward pass
+        transformer_outputs: Float[torch.Tensor, "bs T embed_dim"] = self.backbone(
+            latent_features, mask=mask
+        )
+
+        return transformer_outputs
+
+    def _embed_audio_codes(self, codes_c1: torch.Tensor, codes_c2: torch.Tensor):
+        """
+        Embed the audio codes into latent features.
+        """
+        with torch.no_grad():
+            c1_latent = self.quantizer.decode(codes_c1)
+            c2_latent = self.quantizer.decode(codes_c2)
+
+        # combine the two channels.
+        latent_features: Float[torch.Tensor, "bs D T"] = torch.cat(
+            [c1_latent, c2_latent], dim=1
+        )
+        latent_features: Float[torch.Tensor, "bs T D"] = latent_features.transpose(1, 2)
+
+        # project to match the embedding dimension of the transformer
+        latent_features: Float[torch.Tensor, "bs T embed_dim"] = self.input_proj(
+            latent_features
+        )
+        return latent_features
